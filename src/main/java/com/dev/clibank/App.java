1ff0b4c79c7df80689053441b0937e4a33cdb33b
carmo <carlosh.carmo@hotmail.com>
2024-04-22 21:59:52 -0300

    Included Spring Boot version 2.4.5

--- src/main/java/com/dev/clibank/App.java
+++ src/main/java/com/dev/clibank/App.java
@@ -1,16 +1,19 @@
 package com.dev.clibank;
 
 import com.dev.clibank.app.cli.Menu;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
+
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args ) {
-        Menu menu = new Menu();
-        menu.printMenu();
+        SpringApplication.run(App.class, args);
     }
 
 
